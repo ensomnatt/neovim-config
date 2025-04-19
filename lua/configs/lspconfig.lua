961a351d4dc863e229f4bb7ec91667ba17989eeb
ensomnatt <ensomnatt@protonmail.com>
2025-04-19 18:30:37 +0300

    blablablableblebleblublublubluhaplblbbllblblbbbl

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,4 +1,4 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "gopls", "ts_ls" }
+local servers = { "html", "cssls", "gopls", "ts_ls", "bashls" }
 vim.lsp.enable(servers)
--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,7 @@ require "nvchad.options"
 -- add yours here!
 
 vim.opt.scrolloff = 10
+vim.o.numberwidth = 3
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
