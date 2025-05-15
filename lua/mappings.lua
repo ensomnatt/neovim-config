251e4b4d66532ec6016c7cee9426952c91390412
ensomnatt <ensomnatt@protonmail.com>
2025-05-15 21:36:12 +0300

    update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
+map("n", "<leader>eo", "<CMD>Oil --float .<CR>", { desc = "open oil" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
