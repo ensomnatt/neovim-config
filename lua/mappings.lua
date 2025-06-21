2c3b083bee9237123b1f4d178a228b18e927dc32
ensomnatt <ensomnatt@protonmail.com>
2025-06-22 00:29:44 +0300

    diagnostics

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,5 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
 map("n", "<leader>eo", "<CMD>Oil --float .<CR>", { desc = "open oil" })
+map("n", "gl", "<CMD>lua vim.diagnostic.open_float()<CR>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
